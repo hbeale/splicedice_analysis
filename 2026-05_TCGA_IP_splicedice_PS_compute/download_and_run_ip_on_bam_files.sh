1380e610aa7471c0d89b9a5890408bec9a54522b
hbeale <hbeale@gmail.com>
2026-05-29 12:00:08 -0700

    Create download_and_run_ip_on_bam_files.sh

---
+++ 2026-05_TCGA_IP_splicedice_PS_compute/download_and_run_ip_on_bam_files.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ip_manifest=/mnt/splicedice_ir_example/git_code/splicedice_analysis/2026-05_TCGA_IP_splicedice_PS_compute/dataset_selection/soulette_equivalent_intron_prospector_manifest.2026.05.28.tsv 
+genome=/mnt/ref/GRCh38.primary_assembly.genome.fa
+
+ip_run_dir=/mnt/data/intron_prospector_runs/common/
+bam_base=/mnt/data/tcga
+
+# document IP version
+docker run --rm \
+-v /mnt/:/mnt \
+splicedice_analysis:latest \
+intron-prospector -v
+
+
+cat $ip_manifest | grep -v ^id | while read ugly_id bam_file nice_id; do
+# echo echo ugly_id is $ugly_id
+# echo bam file is $bam_file
+# echo nice_id is $nice_id
+
+bed_file=$ip_run_dir/${nice_id}.bed 
+bam_file=${bam_base}/$ugly_id/$bam_file
+
+echo
+echo checking $nice_id
+if [[ ! -f "$bed_file" && ! -f "$bam_file" ]]; then 
+echo "neither bed or bam file exists; downloading bam file..."
+
+# download bam file
+echo /mnt/scratch/gdc-client download --dir  /mnt/data/tcga --token-file $token_file $ugly_id
+
+# run IP
+echo "running intron-prospector..."
+docker run --rm \
+-v /mnt/:/mnt \
+splicedice_analysis:latest \
+intron-prospector \
+--genome-fasta=$genome \
+--intron-bed6=$bed_file \
+--skip-missing-targets \
+$bam_file
+
+# delete bam file...
+rm $bam_file
+fi
+
+done
+
