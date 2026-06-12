b795f959e4b0e9557049a6d862def1944b8e67d0
hbeale <hbeale@gmail.com>
2026-06-12 12:35:52 -0700

    Create run_ip_on_bam_files_v2.sh

---
+++ 2026-05_TCGA_IP_splicedice_PS_compute/run_ip_on_bam_files_v2.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -lt 1 ]]; then
+    echo "Usage: $0 <ip_manifest>"
+    exit 1
+fi
+
+ip_manifest=$1
+genome=/mnt/ref/GRCh38.primary_assembly.genome.fa
+
+ip_run_dir=/mnt/data/intron_prospector_runs/common/
+
+# document IP version
+docker run --rm \
+-v /mnt/:/mnt \
+splicedice_analysis:latest \
+intron-prospector -v
+
+cat $ip_manifest | grep -v ^id | while read ugly_id bam_file nice_id; do
+# echo echo ugly_id is $ugly_id
+# echo bam file is $bam_file
+# echo nice_id is $nice_id
+
+bed_file=$ip_run_dir/${nice_id}.bed 
+bam_file=/mnt/data/tcga/$ugly_id/$bam_file
+
+if [[ -f "$bam_file" && $(find "$bam_file" -mmin +60) ]]; then
+echo "$bam_file for $nice_id exists and not modified in the last hour"
+if [[ ! -f "$bed_file" ]]; then
+echo "bed file $bed_file for $nice_id does not exist"
+docker run --rm \
+-v /mnt/:/mnt \
+splicedice_analysis:latest \
+intron-prospector \
+--genome-fasta=$genome \
+--intron-bed6=$bed_file \
+--skip-missing-targets \
+$bam_file
+
+fi
+echo 
+fi
+done
+
